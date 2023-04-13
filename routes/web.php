dcf5b2304a0158ea01f1722c1e10acdbbda8ba0a
naufalgitam <naufalps24@gmail.com>
2023-04-13 14:19:31 +0700

    create, read, search data

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
 Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
 Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
 Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
 
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
