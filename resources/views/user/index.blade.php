f23ad57f444b59337b7fbaf6d86dca8c487e27ac
naufalgitam <naufalps24@gmail.com>
2023-05-10 22:50:40 +0700

    update, delete data

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="front-semibold text-x1 text-gray-800 dark:text-gray-200 leading-tight">
-            {{__('Todo') }}
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('User') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="px-6 pt-6 mb-5 md:w-1/2 2xl:w-1/3">
                     @if (request('search'))
                     <h2 class="pb-3 text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
@@ -25,7 +25,6 @@
                 <div class="px-6 text-xl text-gray-900 dark:text-gray-100">
                     <div class="flex items-center justify-between">
                             <div></div>
-
                             <div>
                                 @if (session('success'))
                                 <p x-data="{ show: true}" x-show="show" x-transition
@@ -41,46 +40,43 @@
                                 @endif
                             </div>
                     </div>
-
                 </div>
                 <div class="relative overflow-x-auto">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3">
-                                    Id
+                                                Id
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    Name
+                                                Name
                                 </th>
                                 <th scope="col" class="hidden px-6 py-3 md:block">
-                                    Email
+                                                Email
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    Todo
+                                                Todo
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    Action
+                                                Action
                                 </th>
                             </tr>
                         </thead>
 
                         <tbody>
                             @forelse ($users as $user)
-
                             <tr class="odd:bg-white odd:dark:bg-gray-800 even:bg-gray-50 even:dark:bg-gray-700">
-
                                 <td scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <p>{{ $user->id }}</p>
+                                    class="px-6 py- font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <p>{{ $user->id }}</p>
                                 </td>
 
                                 <td class="px-6 py-4 font-medium text-gray-900 md:whitespace-nowrap dark:text-white">
-                                <p>{{ $user->name }}</p>
+                                    <p>{{ $user->name }}</p>
                                 </td>
 
                                 <td class="hidden px-6 py-4 md:block">
-                                <p>{{ $user->email }}</p>
+                                    <p>{{ $user->email }}</p>
                                 </td>
 
                                 <td class="px-6 py-4 whitespace-nowrap">
@@ -89,8 +85,8 @@
                                             <span class="text-green-600 dark:text-green-400">({{
                                                 $user->todos->where('is_complete', true)->count() }}</span>/
 
-                                            <span class="text-blue-600 dark:text-blue-400">{{
-                                            $user->todos->where('is_complete', false)->count() }})</span>
+                                            <span class="text-blue-600 dark:text-blue-400">{{ 
+                                                $user->todos->where('is_complete', false)->count() }})</span>
                                         </span>
                                     </p>
                                 </td>
@@ -98,14 +94,39 @@
                                 <td class="px-6 py-4">
                                     <div class="flex space-x-3">
 
+                                    @if ($user->is_admin)
+                                        <form action="{{ route('user.removeadmin', $user) }}" method="Post">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit" class="text-blue-600 dark:text-blue-400 whitespace-nowrap">
+                                                                Remove Admin
+                                            </button>
+                                        </form>
+                                        @else
+                                        <form action="{{ route('user.makeadmin', $user) }}" method="Post">
+                                        @csrf
+                                        @method('PATCH')
+                                        <button type="submit" class="text-red-600 dark:text-red-400 whitespace-nowrap">
+                                                                Make Admin
+                                        </button>
+                                        </form>
+                                        @endif
+                                        <form action="{{ route('user.destroy', $user) }}" method="Post">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="submit"
+                                                class="text-red-600 dark:text-red-400 whitespace-nowrap">
+                                                                Delete
+                                            </button>        
+                                        </form>
                                     </div>
                                 </td>
+
                             </tr>
                             @empty
-
                             <tr class="bg-white dark:bg-gray-800">
                                 <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                                    Empty
+                                                Empty
                                 </td>
                             </tr>
                             @endforelse
@@ -117,8 +138,8 @@
                     {{ $users->Links() }}
                 </div>
                 @endif
+                </div>
             </div>
         </div>
     </div>
-
 </x-app-layout>
---
+++ tugas6.md
@@ -0,0 +1,18 @@
+# Tugas 6
+
+## 1. Todo Completed
+![Alt text](screenshoot/tugas6/Todo%20Complete.png/)
+## 2. Todo Uncompleted
+![Alt text](screenshoot/tugas6/Todo%20Uncompleted.png/)
+## 3. Todo Edited
+![Alt text](screenshoot/tugas6/Todo%20Edited.png)
+## 4. Todo Deleted
+![Alt text](screenshoot/tugas6/Todo%20Delete.png/)
+## 5. Todo Deleted All Completed Todo
+![Alt text](screenshoot/tugas6/Todo%20Deleted%20All%20Completed%20Todo.png/)
+## 6. User Make Admin
+![Alt text](screenshoot/tugas6/User%20Make%20Admin.png/)
+## 7. User Remove Admin
+![Alt text](screenshoot/tugas6/User%20Remove%20Admin.png/)
+## 8. User Deleted 
+![Alt text](screenshoot/tugas6/User%20Deleted.png/)
